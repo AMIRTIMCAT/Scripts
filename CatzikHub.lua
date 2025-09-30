7725afc8ed8fed75f7552b1892c965911f30cfe3
AMIRTIMCAT <amirtimcatyt@gmail.com>
2025-09-30 05:29:35 +0500

    Update CatzikHub.lua

--- CatzikHub.lua
+++ CatzikHub.lua
@@ -1,7 +1,24 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/main.luau"))()
 
 local Window = Library:MakeWindow({
-  Title = "Catzik Hub",
-  SubTitle = "by Yoshi",
-  ScriptFolder = "redz-library-V5"
+  Title = "My Cool Hub",
+  SubTitle = "by me",
+  ScriptFolder = "my-scripts"
+})
+
+local Tab = Window:MakeTab({
+  Title = "Main",
+  Icon = "Home"
+})
+
+Tab:AddButton({
+  Name = "Click Me!",
+  Callback = function()
+    print("Button clicked!")
+    Window:Notify({
+      Title = "Hello!",
+      Content = "You clicked the button!",
+      Duration = 3
+    })
+  end
 })
