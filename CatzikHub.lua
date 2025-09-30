eb7275f10b7c5378848712c28923fa2c128a2e7e
AMIRTIMCAT <amirtimcatyt@gmail.com>
2025-09-30 05:25:16 +0500

    Update CatzikHub.lua

--- CatzikHub.lua
+++ CatzikHub.lua
@@ -2,16 +2,8 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlred
 
 local Window = Library:MakeWindow({
   Title = "Catzik Hub",
-  SubTitle = "By Yoshi",
+  SubTitle = "by Yoshi",
   ScriptFolder = "redz-library-V5"
 })
 
-MainTab:AddDiscordInvite({
-	Title = "Catzik Hub",
-	Description = "A community for Catzik Hub Users -- official scripts, updates, and suport in one place.",
-	Banner = "rbxassetid://17382040552", -- You can put an RGB Color: Color3.fromRGB(233, 37, 69)
-	Logo = "rbxassetid://17382040552",
-	Invite = "https://discord.gg/catzik-hub",
-	Members = 470000, -- Optional
-	Online = 20000, -- Optional
-})
+local Tab = Window:MakeTab({ "Cool Tab", "Home" })
