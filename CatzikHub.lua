e306a838a2badca9151ef13bbbf068ebd3ae4937
AMIRTIMCAT <amirtimcatyt@gmail.com>
2025-09-30 05:26:35 +0500

    Update CatzikHub.lua

--- CatzikHub.lua
+++ CatzikHub.lua
@@ -4,5 +4,6 @@ local Window = Library:MakeWindow({
   Title = "Catzik Hub",
   SubTitle = "by Yoshi",
   ScriptFolder = "redz-library-V5"
-    local Tab = Window:MakeTab({ "Cool Tab", "Home" })
+    
+  local Tab = Window:MakeTab({ "Cool Tab", "Home" })
 })
