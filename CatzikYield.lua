84cc9cea0183c7726ec5a369f2d8f20dcad2cc66
AMIRTIMCAT <amirtimcatyt@gmail.com>
2025-10-02 22:19:16 +0500

    Update CatzikYield.lua

--- CatzikYield.lua
+++ CatzikYield.lua
@@ -1,7 +1,3 @@
---========================================
--- Catik Yield GUI (English, global) Improved, no _G
---========================================
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/main.luau"))()
 
 local Window = Library:MakeWindow({
