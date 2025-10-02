971f3be4a06de4c9200903ccb5ea9f3bdffdc758
AMIRTIMCAT <amirtimcatyt@gmail.com>
2025-10-02 22:32:22 +0500

    Update CatzikYield.lua

--- CatzikYield.lua
+++ CatzikYield.lua
@@ -1,4 +1,3 @@
-```lua
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/main.luau"))()
 
 local Window = Library:MakeWindow({
