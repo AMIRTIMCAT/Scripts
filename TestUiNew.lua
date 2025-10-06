16e06c287ebe265259d9fbda4c220fcec737767f
AMIRTIMCAT <amirtimcatyt@gmail.com>
2025-10-07 00:33:50 +0500

    Update and rename FinderVipServer.lua to TestUiNew.lua

---
+++ TestUiNew.lua
@@ -0,0 +1,2 @@
+--link:https://brady-xyz.gitbook.io/maclib-ui-library
+local MacLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/biggaboy212/Public-Resources/main/MacLib/maclib.lua"))() MacLib:Demo()
