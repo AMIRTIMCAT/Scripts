a24576cf1a100b218fae02f6ab0c0027fe577214
AMIRTIMCAT <amirtimcatyt@gmail.com>
2025-10-07 00:39:16 +0500

    Update TestUiNew.lua

--- TestUiNew.lua
+++ TestUiNew.lua
@@ -1,2 +1,13 @@
 --link:https://brady-xyz.gitbook.io/maclib-ui-library
 local MacLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/biggaboy212/Public-Resources/main/MacLib/maclib.lua"))() MacLib:Demo()
+
+local Window = MacLib:Window({
+    Title = "FluxusZ Hub",
+    Subtitle = "Freemuim | V1.00",
+    Size = UDim2.fromOffset(868, 650),
+    DragStyle = 1,
+    DisabledWindowControls = {},
+    ShowUserInfo = true,
+    Keybind = Enum.KeyCode.RightControl,
+    AcrylicBlur = true,
+})
